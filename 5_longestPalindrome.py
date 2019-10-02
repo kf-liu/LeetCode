91a11f63c33b1aa1784d8932e8c5a2e7126172b6
Kelly L <41723241+qiaork@users.noreply.github.com>
2019-10-02 14:33:48 +0800

    Reduce time complexity

--- 5_longestPalindrome.py
+++ 5_longestPalindrome.py
@@ -1,12 +1,25 @@
 class Solution:
   def longestPalindrome(self, s: str) -> str:
     l=len(s)
-    r=l
-    while r>1:
-      for i in range(l-r+1):
-        if s[i:i+r]==s[i:i+r][::-1]:
-          return s[i:i+r]
-      r-=1
-    if r>0:
-      return s[0]
-    return ""
+    if l==0: return ""
+    ans=s[0]
+    r=1
+    i=0
+    while True:
+      i+=1
+      left=int(i/2)+i%2-int(r/2)-r%2
+      right=int(i/2)+int(r/2)+r%2
+      if right>=l: break
+      if s[left]==s[right]:
+        if s[left:right+1]==s[left:right+1][::-1]:
+          #print(r,s[left:right+1])
+          j=1
+          while left-j>=0 and right+j<=l-1:
+            if s[left-j]==s[right+j]:
+              j+=1
+              #print(r,j,s[left-j+1:right+j])
+            else: break
+          ans=s[left-j+1:right+j]
+          r=len(ans)
+    return ans
+#140ms,89.73%
